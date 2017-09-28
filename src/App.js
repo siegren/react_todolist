5e4cd933c22c2120a23d17128a9ca74398786535
deazee101 <deazee101@gmail.com>
2017-09-28 09:41:50 +0800

    research

--- src/App.js
+++ src/App.js
@@ -8,22 +8,39 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state ={
-      todos: ['watch ', 'eat']
+      todos: ['watch ', 'eat'],
+      search: ['watch','eat']
     }
   }
 
   addNewTodo(todo){
     console.log(todo)
     this.setState({
-      todos:this.state.todos.concat(todo)
+      todos: this.state.todos.concat(todo)
     });
+      this.setState({
+        search: this.state.search.concat(todo)
+      });
   }
+
+  search(event){
+var items= this.state.todos;
+var filterItems = items.filter(function(todo){
+  return todo.includes(event.target.value);
+});
+this.setState({
+  search: filterItems
+})
+  }
+
+
   render() {
     return (
       <div>
       <h1> my Todos </h1>
       <TodoForm addNewTodoFunc={this.addNewTodo.bind(this)}/>
-      <TodoList todo_data={this.state.todos}/>
+      <TodoList todo_data={this.state.search}/>
+      <input placeholder="search...." onChange={(event) => this.search(event)}/>
       </div>
     );
   }
